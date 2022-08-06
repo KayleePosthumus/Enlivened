3d84b0a75d0c0be1b7066d2100d835d62a5997f6
LarisaBothaTuks <u20522623@tuks.co.za>
2022-08-06 20:06:44 +0200

    🚧 (Map): Incomplete map stuff

---
+++ client/pages/map.js
@@ -0,0 +1,18 @@
+const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM()
+      })
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2
+    })
+  });
+
+
+//pass in marker type, image
+function addMarker(){
+
+}
