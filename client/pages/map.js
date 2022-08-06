01710a1cc585301240a4180f9390a38cb06ceec5
LarisaBothaTuks <u20522623@tuks.co.za>
2022-08-06 23:49:30 +0200

    login frontend

--- client/pages/map.js
+++ client/pages/map.js
@@ -1,3 +1,8 @@
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+//LOAD MAP
 const map = new Map({
     target: 'map',
     layers: [
@@ -11,8 +16,48 @@ const map = new Map({
     })
   });
 
+//MARKER TYPES
+var pastMarkerLayer = new ol.layer.Vector({
+  source: new ol.source.Vector(),
+  style: new ol.style.Style({
+    image: new ol.style.Icon({
+      src: '../markers/default.png'
+    })
+  })
+});
+map.addLayer(pastMarkerLayer);
 
-//pass in marker type, image
-function addMarker(){
+var upcomingMarkerLayer = new ol.layer.Vector({
+  source: new ol.source.Vector(),
+  style: new ol.style.Style({
+    image: new ol.style.Icon({
+      src: '../markers/default.png'
+    })
+  })
+});
+map.addLayer(upcomingMarkerLayer);
 
+//Add Markers
+
+function addPastMarker(long,lat){
+  var marker = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([long, lat])));
+  pastMarkerLayer.getSource().addFeature(marker);
 }
+
+function addUpMarker(long,lat){
+  var marker = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([long, lat])));
+  upcomingMarkerLayer.getSource().addFeature(marker);
+
+  marker.events.register('click', marker, function (evt) {
+    alert("lon: "+evt.object.lonlat.lon+" , lat: "+evt.object.lonlat.lon);
+  });
+}
+
+//Usage Ex
+
+addPastMarker(28.229271,-25.747868);
+addUpMarker(27.229271,-21.747868);
+
+// map.on('click', function(evt){
+//   alert(ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'));
+// });
