5256c9b5b21ae67133d00581edb9af26a5f4a09f
LarisaBothaTuks <u20522623@tuks.co.za>
2022-08-07 09:32:19 +0200

    fix

--- client/pages/map.js
+++ client/pages/map.js
@@ -132,7 +132,7 @@ addCarMarker(20.187,-25.4,"hey");
 addIslamMarker(17.229271,-29.747868,"hey bitch");
 addSupport2Marker(18.229271,-23.747868,"hey bitch");
 
-document.getElementById("createEvent").onclick = function() {
+document.getElementById("asd").onclick = function() {
   map.on('click', function(evt){
     alert(ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326'));
   });
