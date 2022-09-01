c5a1265d32c0ba8d389fc93fa967ff8b14212335
Kajal022 <u20554240@tuks.co.za>
2022-09-02 00:09:52 +0200

    added logo to nav bar

--- frontend/src/pages/navbar.jsx
+++ frontend/src/pages/navbar.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import '../style.css';
 const Navbar = (currentPage) => {
     return (
 
       <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
         <div class="container-fluid">
 
-            <div class="container-fluid">
-                <a class="navbar-brand" href="./home.html">
-                    <img src="../img/enlivened-dark.svg" alt="" width="130"/>
+        <div class="container-fluid">
+                <a class="navbar-brand" href="./home">
+                    
+                        <img src={ require('../logo/enlivened.png')} alt="" width="160" />
+                    
+                    
                 </a>
             </div>
             
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> 
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
